e70a108942573930f9629ab29cbda3a58f205be7
Arthur Signorelli <164225457+ArthurSignorelli@users.noreply.github.com>
2025-07-09 17:48:20 -0300

    Add files via upload

--- context/GastoContext.js
+++ context/GastoContext.js
@@ -1,1 +1,18 @@
+import React, { createContext, useState } from 'react';
 
+export const GastoContext = createContext();
+
+export const GastoProvider = ({ children }) => {
+  const [gastos, setGastos] = useState([]);
+  const [perfil, setPerfil] = useState({ nome: '', email: '' });
+
+  const adicionarGasto = (gasto) => {
+    setGastos([...gastos, gasto]);
+  };
+
+  return (
+    <GastoContext.Provider value={{ gastos, adicionarGasto, perfil, setPerfil }}>
+      {children}
+    </GastoContext.Provider>
+  );
+};
